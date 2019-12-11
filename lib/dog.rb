6cd8f9ef3a104906a17197986baa8644eb4cf58e
Sean Beach <sean.beach1@yahoo.com>
2019-12-11 13:45:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,10 @@
 class Dog(name, breed)
-
-    @name = name
-  end
   def name
     @name
   end
     def name=(new_name)
       @name = new_name
     end
-  def initialize(breed)
-    @breed = breed
-  end
   def breed
     @breed
   end
