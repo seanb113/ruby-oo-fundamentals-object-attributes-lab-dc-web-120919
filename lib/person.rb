ab6a3dd710aecf571f12eca53a184eb63b8a4ae6
Sean Beach <sean.beach1@yahoo.com>
2019-12-11 13:44:35 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,14 +1,10 @@
 class Person
-  def initialize(name)
   def name
     @name
   end
     def name= (new_name)
       @name = new_name
     end
-  def initialize(job)
-    @job = job
-  end
   def job
     @job
   end
